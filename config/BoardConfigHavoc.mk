ca21f210ee7b0ab758c964ef8f5a9d0228a5699e
AmebixElo <59183853+AmebixElo@users.noreply.github.com>
2019-12-24 12:17:42 +0100

    Update BoardConfigHavoc.mk

--- config/BoardConfigHavoc.mk
+++ config/BoardConfigHavoc.mk
@@ -3,10 +3,10 @@ ifeq ($(WITH_HAVOC_CHARGER),true)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.havoc
 endif
 
-include vendor/havoc/config/BoardConfigKernel.mk
+include vendor/united/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include vendor/havoc/config/BoardConfigQcom.mk
+include vendor/united/config/BoardConfigQcom.mk
 endif
 
-include vendor/havoc/config/BoardConfigSoong.mk
+include vendor/united/config/BoardConfigSoong.mk
